9ae712888b4c8add2df5a02289c7920d06c7c853
Fatma R <anthoschichequs@gmail.com>
2025-07-18 17:09:50 +0300

    degisiklikler yapildi.

--- database/migrations/2025_07_18_141307_add_is_favorited_to_notes_table.php
+++ database/migrations/2025_07_18_141307_add_is_favorited_to_notes_table.php
@@ -6,23 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        Schema::table('{{ table }}', function (Blueprint $table) {
-            //
+        Schema::table('notes', function (Blueprint $table) {
+            $table->boolean('is_favorited')->default(false)->after('archived_at');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('{{ table }}', function (Blueprint $table) {
-            //
+        Schema::table('notes', function (Blueprint $table) {
+            $table->dropColumn('is_favorited');
         });
     }
 };
